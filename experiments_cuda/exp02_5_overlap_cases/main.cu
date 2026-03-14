7cacdde5c673dc2d704a731a0424711b5d6bf039
MouseCastle <yoon080693@gmail.com>
2026-03-14 22:34:42 +0900

    add: added Case D pinned_copy_overlap experiment to exp02_5

--- experiments_cuda/exp02_5_overlap_cases/main.cu
+++ experiments_cuda/exp02_5_overlap_cases/main.cu
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <numeric>
+#include <iostream>
+#include <functional>
 
 #include "common/cuda_check.cuh"
 #include "common/cuda_timer.cuh"
@@ -21,7 +23,8 @@ enum class CaseType {
     A_pageable_single_single = 0,
     B_pinned_single_single   = 1,
     C_pinned_two_single      = 2,
-    D_pinned_two_double      = 3
+    D_pinned_two_double      = 3,
+    E_pinned_copy_overlap    = 4
 };
 
 struct Args {
@@ -51,6 +54,7 @@ static Args parse_args(int argc, char** argv)
             else if (v == "B") a.case_type = CaseType::B_pinned_single_single;
             else if (v == "C") a.case_type = CaseType::C_pinned_two_single;
             else if (v == "D") a.case_type = CaseType::D_pinned_two_double;
+            else if (v == "E") a.case_type = CaseType::E_pinned_copy_overlap;
         }
     }
     return a;
@@ -63,6 +67,7 @@ static const char* case_name(CaseType c)
     case CaseType::B_pinned_single_single:   return "B_pinned_single_single";
     case CaseType::C_pinned_two_single:      return "C_pinned_two_single";
     case CaseType::D_pinned_two_double:      return "D_pinned_two_double";
+    case CaseType::E_pinned_copy_overlap:    return "E_pinned_copy_overlap";
     default: return "unknown";
     }
 }
@@ -95,6 +100,12 @@ int main(int argc, char** argv)
         case CaseType::D_pinned_two_double:
             case_ = {true, 2, 2};
             break;
+        case CaseType::E_pinned_copy_overlap:
+            case_ = {true, 2, 2};
+            break;
+        default:
+            std::fprintf(stderr, "Invalid case type\n");
+            return 1;
     }
 
     // Host buffers
@@ -153,26 +164,61 @@ int main(int argc, char** argv)
 
     const bool is_double_buffer = case_.num_buffers == 2;
 
-    auto one_iteration = [&](int iter) {
-        const int b = iter % case_.num_buffers;
+    std::function<void(int)> one_iteration = nullptr;
+    
+    if (args.case_type == CaseType::E_pinned_copy_overlap) {
+        /*
+            In this case, we want to overlap the HtoD copy of the next buffer
+            with the kernel execution of the current buffer.
+        */
+        one_iteration = [&](int iter) {
+            if (case_.num_buffers != 2) {
+                std::fprintf(stderr, "Case E requires 2 buffers\n");
+                std::exit(1);
+            }
 
-        cudaStream_t& stream_copy = is_double_buffer ? stream[b] : stream[0];
-        cudaStream_t& stream_compute = is_double_buffer ? stream[b] : stream[1];
+            // HtoD
+            for (int i = 0; i < case_.num_buffers; ++i) {
+                CUDA_CHECK(cudaMemcpyAsync(dx[i], hx[i], bytes, cudaMemcpyHostToDevice, stream[i]));
+                CUDA_CHECK(cudaEventRecord(copy_done[i], stream[i]));
+            }
+            for (int i = 0; i < case_.num_buffers; ++i) {            
+                CUDA_CHECK(cudaStreamWaitEvent(stream[i], copy_done[i], 0));
+            }
 
-        // HtoD
-        CUDA_CHECK(cudaMemcpyAsync(dx[b], hx[b], bytes, cudaMemcpyHostToDevice, stream_copy));
-        CUDA_CHECK(cudaEventRecord(copy_done[b], stream_copy));
+            // Kernel dependency on HtoD
+            for (int i = 0; i < case_.num_buffers; ++i) {
+                axpby_kernel<<<grid, block, 0,  stream[i]>>>(dx[i], dy[i], n, 1.1f, 2.2f);
+                CUDA_CHECK(cudaGetLastError());
+            }
+            
+            // DtoH
+            for (int i = 0; i < case_.num_buffers; ++i) {
+                CUDA_CHECK(cudaMemcpyAsync(hy[i], dy[i], bytes, cudaMemcpyDeviceToHost, stream[i]));
+            }
+        };
+    } else {
+        one_iteration = [&](int iter) {
+            const int b = iter % case_.num_buffers;
 
-        // Kernel dependency on HtoD
-        if (args.case_type == CaseType::C_pinned_two_single) {
-            CUDA_CHECK(cudaStreamWaitEvent(stream_compute, copy_done[b], 0));
-        }
+            cudaStream_t& stream_copy = is_double_buffer ? stream[b] : stream[0];
+            cudaStream_t& stream_compute = is_double_buffer ? stream[b] : stream[1];
 
-        axpby_kernel<<<grid, block, 0, stream_compute>>>(dx[b], dy[b], n, 1.1f, 2.2f);
-        CUDA_CHECK(cudaGetLastError());
+            // HtoD
+            CUDA_CHECK(cudaMemcpyAsync(dx[b], hx[b], bytes, cudaMemcpyHostToDevice, stream_copy));
+            CUDA_CHECK(cudaEventRecord(copy_done[b], stream_copy));
 
-        CUDA_CHECK(cudaMemcpyAsync(hy[b], dy[b], bytes, cudaMemcpyDeviceToHost, stream_copy));
-    };
+            // Kernel dependency on HtoD
+            if (args.case_type == CaseType::C_pinned_two_single) {
+                CUDA_CHECK(cudaStreamWaitEvent(stream_compute, copy_done[b], 0));
+            }
+
+            axpby_kernel<<<grid, block, 0, stream_compute>>>(dx[b], dy[b], n, 1.1f, 2.2f);
+            CUDA_CHECK(cudaGetLastError());
+
+            CUDA_CHECK(cudaMemcpyAsync(hy[b], dy[b], bytes, cudaMemcpyDeviceToHost, stream_copy));
+        };
+    }
 
     // Warmup
     for (int i = 0; i < args.warmup; ++i) {
@@ -184,12 +230,19 @@ int main(int argc, char** argv)
     CudaEventTimer gt;
     CpuTimer ct;
 
-    cudaStream_t& last_stream = is_double_buffer ? stream[args.iters % 2 + 1] : stream[1];
+    cudaStream_t last_stream = is_double_buffer ? stream[args.iters % 2 + 1] : stream[1];
+
+    int iters = args.iters;
+    if (args.case_type == CaseType::E_pinned_copy_overlap) {
+        // For the overlap case, we need to run one extra iteration to fully utilize the pipeline
+        last_stream = stream[1]; // The last kernel will be on stream[1]
+        iters /= case_.num_buffers;
+    }
 
     ct.tic();
     gt.tic(last_stream);
 
-    for (int i = 0; i < args.iters; ++i) {
+    for (int i = 0; i < iters; ++i) {
         one_iteration(i);
     }
 
@@ -205,10 +258,10 @@ int main(int argc, char** argv)
         checksum += hy[i][n - 1];
     }
 
-    const double total_elems = double(n) * double(args.iters);
+    const double total_elems = double(n) * double(iters);
     const double sec = cpu_ms / 1000.0;
     const double gelem_per_s = (total_elems / sec) / 1e9;
-    const double avg_iter_ms = cpu_ms / double(args.iters);
+    const double avg_iter_ms = cpu_ms / double(iters);
 
     std::printf("case=%s\n", case_name(args.case_type));
     std::printf("batch=%d elements=%d n=%d buffers=%d pinned=%d num_streams=%d\n",
@@ -226,7 +279,7 @@ int main(int argc, char** argv)
     std::snprintf(row, sizeof(row),
                   "%s,%d,%d,%d,%d,%d,%d,%d,%.6f,%.6f,%.6f,%.6f,%.6f",
                   case_name(args.case_type),
-                  args.batch, args.elements, args.iters, args.warmup,
+                  args.batch, args.elements, iters, args.warmup,
                   case_.num_buffers, (int)case_.pinned, case_.num_streams,
                   (double)gpu_ms, cpu_ms, avg_iter_ms, gelem_per_s, checksum);
 
